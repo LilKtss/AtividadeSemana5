93a190b2a42aac2a0b754b827c59c4a7fa01811f
LilKtss <lucas.barros.palmeira15@gmail.com>
2025-09-21 21:35:50 -0300

    first commit

---
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.suacalculadora // O nome do seu pacote pode variar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calculadora.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
